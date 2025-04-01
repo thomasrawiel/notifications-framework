21c1d9b4df432e3870016a3e8dcf7915c28ff48b
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2025-04-01 16:26:14 +0200

    setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # notifications-framework
 Framework for TYPO3 notifications
+
+
+> [!WARNING]  
+> This is currenlty just a prototype, do not use in production environment!
---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Notifications Framework',
+    'description' => '',
+    'state' => 'alpha',
+    'category' => 'misc',
+    'author' => 'Thomas Rawiel',
+    'author_email' => 'thomas.rawiel@gmail.com',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
