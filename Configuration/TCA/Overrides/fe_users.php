70425a11bff0ec28b4fea68916d0d49855335a88
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2025-07-22 16:54:52 +0200

    [feat] store notification settings

---
+++ Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,16 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', [
+    'notifications' => [
+        'label' => 'Notifications',
+        'config' => [
+            'type' => 'passthrough',
+            'default' => 0,
+        ],
+    ],
+]);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'fe_users',
+    'notifications',
+
+);
--- ext_tables.sql
+++ ext_tables.sql
@@ -29,3 +29,8 @@ CREATE TABLE `tx_notifications_framework_domain_model_notification`
     `image`         int(11) unsigned DEFAULT '0' NOT NULL,
     `url`           varchar(1024)    DEFAULT ''  NOT NULL
 );
+
+CREATE TABLE fe_users
+(
+    `notifications` tinyint(8) unsigned DEFAULT '0' NOT NULL
+);
